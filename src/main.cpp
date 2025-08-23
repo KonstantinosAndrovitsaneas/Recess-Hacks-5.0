312bd2ae44c3d394ce4f7d4d114aaed613543c14
ahmadwaheedshawkat@gmail.com <ahmadwaheedshawkat@gmail.com>
2025-08-23 18:31:08 -0400

    a

--- src/main.cpp
+++ src/main.cpp
@@ -18,8 +18,6 @@ int main() {
 
     InitWindow(screenWidth, screenHeight, "raylib [core] example - keyboard input");
 
-    Vector2 ballPosition = { (float)screenWidth / 2, (float)screenHeight / 2 };
-
     SetTargetFPS(60);               // Set our game to run at 60 frames-per-second
     //--------------------------------------------------------------------------------------
 
@@ -51,10 +49,7 @@ int main() {
     {
         // Update
         //----------------------------------------------------------------------------------
-        if (IsKeyDown(KEY_RIGHT)) ballPosition.x += 2.0f;
-        if (IsKeyDown(KEY_LEFT)) ballPosition.x -= 2.0f;
-        if (IsKeyDown(KEY_UP)) ballPosition.y -= 2.0f;
-        if (IsKeyDown(KEY_DOWN)) ballPosition.y += 2.0f;
+       
         //----------------------------------------------------------------------------------
 
         // Draw
@@ -63,9 +58,6 @@ int main() {
 
         ClearBackground(RAYWHITE);
 
-        DrawText("move the ball with arrow keys", 10, 10, 20, DARKGRAY);
-
-        DrawCircleV(ballPosition, 50, MAROON);
 
         //grid.RenderTileSet();
 
